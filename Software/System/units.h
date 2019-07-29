3b9560cfb8ac90f5f7ec9fc39408f7e5efde4e5d
Zane Cersovsky <zcersovsky@gmail.com>
2019-07-28 20:12:11 -0500

    Added more unit literals

--- Software/System/units.h
+++ Software/System/units.h
@@ -5,3 +5,13 @@ constexpr size_t operator ""_MB(unsigned long long rawValue)
 {
     return 1024 * 1024 * rawValue;
 }
+
+constexpr size_t operator ""_khz(unsigned long long rawValue)
+{
+    return 1000 * rawValue;
+}
+
+constexpr size_t operator ""_Mhz(unsigned long long rawValue)
+{
+    return 1000_khz * rawValue;
+}
